77df5d4abbdc99ef5526d2bb6ac941477ceabbbb
Binu Jasim <bnjasim@gmail.com>
2017-03-06 18:21:12 +0530

    Basic display of retrieved articles titles

--- static/write/writer.js
+++ static/write/writer.js
@@ -8,7 +8,16 @@ $(document).ready(function(){
 			type: 'GET',
 
 			success: function(received_data){
-				console.log(received_data);
+				data = JSON.parse(received_data);
+				//console.log(received_data);
+				var article_list = '<ol>';
+				for (var i=0; i<data.length; i++) {
+					var datum = data[i];
+					article_list += '<li>' + datum['title'] + '</li>';
+				}
+				article_list += '</ol>';
+				// Insert to the html 
+				$('#display-articles').html(article_list);
 			},
 			error: function(e){
 				console.log('Error' + e);
