a077c5d1ea27b983297179b4678ccb0f0e6ef32d
Binu Jasim <bnjasim@gmail.com>
2017-03-06 19:43:43 +0530

    Display title, description, lastedited etc. of retreived articles

--- static/write/writer.js
+++ static/write/writer.js
@@ -10,12 +10,19 @@ $(document).ready(function(){
 			success: function(received_data){
 				data = JSON.parse(received_data);
 				//console.log(received_data);
-				var article_list = '<ol>';
+				var article_list = '';
 				for (var i=0; i<data.length; i++) {
 					var datum = data[i];
-					article_list += '<li>' + datum['title'] + '</li>';
+					article_list += '<div class="article-list-div">' + 
+						'<div class="article-title">' + datum['title'] + '</div>' + 
+						'<div class="article-sub-lining">' + 
+							'<div class="article-kind">' + datum['kind'] + '</div>' +
+						    'last edited: ' + datum['lastEdited'] +
+						'</div>' +
+						'<div class="article-description">' + datum['description'] + '</div>' +
+						'</div>';
 				}
-				article_list += '</ol>';
+				// article_list += '';
 				// Insert to the html 
 				$('#display-articles').html(article_list);
 			},
