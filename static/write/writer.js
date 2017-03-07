69a6dfad0ec1230eab3350792e550e2c012cd0e5
Binu Jasim <bnjasim@gmail.com>
2017-03-07 07:52:16 +0530

    Redirect to writedown with id

--- static/write/writer.js
+++ static/write/writer.js
@@ -11,21 +11,21 @@ $(document).ready(function(){
 		success: function(received_data){
 			data = JSON.parse(received_data);
 			//console.log(received_data);
-			var article_list = '';
+			
 			for (var i=0; i<data.length; i++) {
 				var datum = data[i];
-				article_list += '<div class="article-list-div">' + 
+				var one_article = $('<div class="article-list-div">' + 
 					'<div class="article-title">' + datum['title'] + '</div>' + 
 					'<div class="article-sub-lining">' + 
 						'<div class="article-kind">' + datum['kind'] + '</div>' +
 						'last edited: ' + datum['lastEdited'] +
 					'</div>' +
 					'<div class="article-description">' + datum['description'] + '</div>' +
-					'</div>';
+					'</div>');
+				one_article.data('id', datum.id);	
+				$('#display-articles').append(one_article);	
 			}
-			// article_list += '';
-			// Insert to the html 
-			$('#display-articles').html(article_list);
+			
 		},
 		error: function(e){
 			console.log('Error' + e);
@@ -35,8 +35,8 @@ $(document).ready(function(){
 	
 	// Click on previous article title should lead to writedown page
 	$('#display-articles').on('click', '.article-title', function() {
-		console.log('clicked')
-		window.location.href = '../writedown'
+		
+		window.location.href = '../writedown?id='+$(this).parent().data('id');
 	});	
 	
 })
