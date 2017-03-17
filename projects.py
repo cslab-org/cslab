2b9383e4d8ab9acc6e46010a428394dc51ca20eb
Binu Jasim <bnjasim@gmail.com>
2017-03-17 22:40:31 +0530

    get rid off separate template folders for blog, project etc. all in templates

--- projects.py
+++ projects.py
@@ -7,7 +7,7 @@ import jinja2
 
 from model import *
 
-template_dir = os.path.join(os.path.dirname(__file__), 'blog-articles')
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),  autoescape = True)
 
 class Handler(webapp2.RequestHandler):
